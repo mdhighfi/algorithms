f39259910857ed5dff8b9af391eeeeee7e1153cc
mdhighfi <matthew.highfield@gmail.com>
2015-12-16 15:10:50 -0800

    Created multiple node types

---
+++ data structures/graph/node.rb
@@ -0,0 +1,14 @@
+class Node
+  attr_reader :value
+end
+
+class TreeNode < Node
+  attr_reader :value, :parent, :leftChild, :rightChild
+
+  def initialize
+  end
+end
+
+class GraphNode < Node
+  
+end
