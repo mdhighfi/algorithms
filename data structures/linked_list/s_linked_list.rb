431446bef4c4339cc0ad9d7bc85e78675d31553e
mdhighfi <matthew.highfield@gmail.com>
2016-01-26 21:37:45 -0800

    Push, last, empty?

--- data structures/linked_list/s_linked_list.rb
+++ data structures/linked_list/s_linked_list.rb
@@ -7,4 +7,42 @@ class SList
     @head = (arr.empty? ? nil : SNode.new(arr.first))
     end
   end
+
+  def push(val)
+    last = last_node
+    if empty?
+      head = SNode.new(val)
+    else
+      last.next = SNode.new(val)
+    end
+  end
+
+  def pop(val)
+
+  end
+
+  def shift(val)
+  end
+
+  def unshift(val)
+  end
+
+  private
+
+  def last_node
+    if head.nil?
+      nil
+    else
+      curr = head
+      until curr.next.nil?
+        curr = curr.next
+      end
+      curr
+    end
+  end
+
+  def empty?
+    head.nil?
+  end
+
 end
